044e8bc03d76c2b97460e52dcdeb48907f9b2f63
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-28 20:13:50 +0200

    Add DisplayFields

---
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/displayfield/FieldKeys.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator.displayfield;
+
+/**
+ * @author Tibor Kovács
+ *
+ */
+public class FieldKeys {
+
+	public static final String ADD_TO_ALL_PARENT = "add-to-all-parent";
+
+	public static final String ADD_TO_INNERMOST_PARENT =
+		"add-to-innermost-parent";
+
+	public static final String ADD_TO_PARENT = "add-to-parent";
+
+	public static final String ADD_TO_RANDOM_PARENT = "add-to-random-parent";
+
+	public static final String COUNT = "count";
+
+	public static final String DEPTH = "depth";
+
+	public static final String SUB_COUNT = "sub-count";
+
+	public static final String UPDATE_LEVEL = "update-level";
+
+}
