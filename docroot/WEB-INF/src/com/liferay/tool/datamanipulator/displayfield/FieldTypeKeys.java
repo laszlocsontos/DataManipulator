6779319bdae3128c48c59d06e293372521889cc8
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-28 20:13:51 +0200

    Add DisplayFields

---
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/displayfield/FieldTypeKeys.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator.displayfield;
+
+/**
+ * @author Tibor Kovács
+ *
+ */
+public class FieldTypeKeys {
+
+	public static final String CHECK_BOX = "check-box";
+
+	public static final String DATE = "date";
+
+	public static final String FILE = "file";
+
+	public static final String FILE_REQUIRED = "required-file";
+
+	public static final String HIDDEN = "hidden";
+
+	public static final String INPUT = "input";
+
+	public static final String INPUT_REQUIRED = "required-input";
+
+	public static final String LABEL = "label";
+
+	public static final String MULTI_SELECT_LIST = "multi-select-list";
+
+	public static final String MULTI_SELECT_ORGANIZATION_LIST =
+		"multi-select-organization-list";
+
+	public static final String MULTI_SELECT_ROLE_LIST =
+		"multi-select-role-list";
+
+	public static final String MULTI_SELECT_SITE_LIST =
+		"multi-select-site-list";
+
+	public static final String MULTI_SELECT_USER_LIST =
+		"multi-select-user-list";
+
+	public static final String MULTI_SELECT_USERGROUP_LIST =
+		"multi-select-usergroup-list";
+
+	public static final String SELECT_LIST = "select-list";
+
+	public static final String SEPARATOR = "separator";
+
+	public static final String TIME = "time";
+
+}
