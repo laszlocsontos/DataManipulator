3d383a96aa5e1c81c8e3770e89eb3ab70ca686c5
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-28 20:09:39 +0200

    Build services

---
+++ docroot/WEB-INF/service/com/liferay/tool/datamanipulator/NoSuchDataManipulatorException.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator;
+
+import com.liferay.portal.NoSuchModelException;
+
+/**
+ * @author Tibor Kovács
+ */
+public class NoSuchDataManipulatorException extends NoSuchModelException {
+
+	public NoSuchDataManipulatorException() {
+		super();
+	}
+
+	public NoSuchDataManipulatorException(String msg) {
+		super(msg);
+	}
+
+	public NoSuchDataManipulatorException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public NoSuchDataManipulatorException(Throwable cause) {
+		super(cause);
+	}
+
+}
