976020e9127fc7bb60b77578195434aa65bcb1a5
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-29 16:49:35 +0200

    Add User generation

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/PortletHandlerFactory.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/PortletHandlerFactory.java
@@ -25,6 +25,7 @@ import com.liferay.tool.datamanipulator.datatype.journal.JournalHandler;
 import com.liferay.tool.datamanipulator.datatype.layout.LayoutHandler;
 import com.liferay.tool.datamanipulator.datatype.messageboards.MessageBoardsHandler;
 import com.liferay.tool.datamanipulator.datatype.organization.OrganizationHandler;
+import com.liferay.tool.datamanipulator.datatype.user.UserHandler;
 import com.liferay.tool.datamanipulator.datatype.wiki.WikiHandler;
 import com.liferay.tool.datamanipulator.entry.EntryTypeKeys;
 import com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel;
@@ -76,6 +77,10 @@ public final class PortletHandlerFactory {
 
 		if (entryTypeKey.equals(EntryTypeKeys.PORTAL_ORGANIZATION)) {
 			return new OrganizationHandler();
+		}
+
+		if (entryTypeKey.equals(EntryTypeKeys.PORTAL_USER)) {
+			return new UserHandler();
 		}
 
 		return null;
