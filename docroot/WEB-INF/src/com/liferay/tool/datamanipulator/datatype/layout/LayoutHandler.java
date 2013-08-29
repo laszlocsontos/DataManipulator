ad389c8713d5504d44f1c3f9fd0bd43d8fb79452
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-29 14:54:06 +0200

    Add Layour generation

---
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/layout/LayoutHandler.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator.datatype.layout;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.tool.datamanipulator.entry.BaseEntry;
+import com.liferay.tool.datamanipulator.entry.EntryTypeKeys;
+import com.liferay.tool.datamanipulator.entryreader.EntryTypeReader;
+import com.liferay.tool.datamanipulator.entryreader.util.EntryReaderUtil;
+import com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel;
+import com.liferay.tool.datamanipulator.handler.portlethandler.AbstractPortletHandler;
+import com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel;
+import com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor;
+
+/**
+ * @author Tibor Kovács
+ *
+ */
+public class LayoutHandler extends AbstractPortletHandler implements
+		PortletHandlerModel {
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel#startErase(com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public void startErase(RequestProcessor requestProcessor)
+		throws PortalException, SystemException {
+
+		// TODO Auto-generated method stub
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel#startGenerate(com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public void startGenerate(RequestProcessor requestProcessor)
+		throws PortalException, SystemException {
+
+		int layoutCount = requestProcessor.getCount(
+			EntryTypeKeys.GENERAL_LAYOUT_ENTRY);
+
+		int layoutDepth = requestProcessor.getDepth(
+			EntryTypeKeys.GENERAL_LAYOUT_ENTRY);
+
+		int layoutSubCount = requestProcessor.getSubCount(
+			EntryTypeKeys.GENERAL_LAYOUT_ENTRY);
+
+		EntryTypeReader layoutEntryType = EntryReaderUtil.getEntryType(
+			EntryTypeKeys.GENERAL_LAYOUT_ENTRY);
+
+		BaseEntry layoutEntry = new BaseEntry(layoutEntryType);
+
+		EntryHandlerModel layoutHandler = new LayoutEntryHandler(
+			layoutCount, 0, layoutDepth, layoutSubCount, layoutEntry, null,
+			requestProcessor);
+
+		layoutHandler.generateEntries((long)0);
+	}
+
+}
