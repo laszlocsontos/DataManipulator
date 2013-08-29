684d4aa031db72499a13974ebd9043506dd72d77
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-29 15:47:01 +0200

    Add Mesasge Boards Category/Thread/Message generation

---
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/messageboards/MessageBoardsDisplayFields.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator.datatype.messageboards;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.tool.datamanipulator.displayfield.DisplayFields;
+import com.liferay.tool.datamanipulator.displayfield.Field;
+import com.liferay.tool.datamanipulator.entry.EntryTypeKeys;
+
+import java.util.List;
+
+/**
+ * @author Tibor Kovács
+ *
+ */
+public class MessageBoardsDisplayFields {
+	public static List<Field> getDisplayFields() throws SystemException {
+		DisplayFields fields = new DisplayFields();
+
+		fields.addUserMultiSelect();
+		fields.addSeparator();
+
+		fields.addCount(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_CATEGORY);
+		fields.addUpdateLevel(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_CATEGORY);
+		fields.addDepth(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_CATEGORY);
+		fields.addSubCount(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_CATEGORY);
+		fields.addSeparator();
+
+		fields.addCount(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_THREAD);
+		fields.addUpdateLevel(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_THREAD);
+		fields.addToParent(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_THREAD);
+		fields.addSeparator();
+
+		fields.addCount(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_MESSAGE);
+		fields.addUpdateLevel(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_MESSAGE);
+		fields.addToParent(EntryTypeKeys.GENERAL_MESSAGE_BOARDS_MESSAGE);
+		fields.addSeparator();
+
+		return fields.getDisplayFields();
+	}
+
+}
