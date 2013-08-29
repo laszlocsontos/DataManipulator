1c8b75210d8311bd1eebb7719009d58f8ea05525
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-29 16:49:44 +0200

    Add User generation

---
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/user/UserHandler.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator.datatype.user;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.tool.datamanipulator.entry.BaseEntry;
+import com.liferay.tool.datamanipulator.entry.EntryTypeKeys;
+import com.liferay.tool.datamanipulator.entryreader.EntryTypeReader;
+import com.liferay.tool.datamanipulator.entryreader.util.EntryReaderUtil;
+import com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel;
+import com.liferay.tool.datamanipulator.handler.portlethandler.AbstractPortletHandler;
+import com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel;
+import com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor;
+
+/**
+ * @author Tibor Kovács
+ *
+ */
+public class UserHandler extends AbstractPortletHandler implements
+		PortletHandlerModel {
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel#startErase(com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public void startErase(RequestProcessor requestProcessor)
+			throws PortalException, SystemException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel#startGenerate(com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public void startGenerate(RequestProcessor requestProcessor)
+		throws PortalException, SystemException {
+
+		int userCount = requestProcessor.getCount(EntryTypeKeys.PORTAL_USER);
+
+		EntryTypeReader userEntryType = EntryReaderUtil.getEntryType(
+			EntryTypeKeys.PORTAL_USER);
+
+		BaseEntry userEntry = new BaseEntry(userEntryType);
+
+		EntryHandlerModel userHandler = new UserEntryHandler(
+			userCount, 0, 0, 0, userEntry, null, requestProcessor);
+
+		userHandler.generateEntries((long)0);
+	}
+
+}
