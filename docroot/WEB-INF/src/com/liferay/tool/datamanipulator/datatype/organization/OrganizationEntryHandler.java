fae40cfa39b69affa3cb49877cc08f2ae77171ee
tiborkovacs <tibor.kovacs@liferay.com>
2013-08-29 16:30:00 +0200

    Add Organization generation

---
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/organization/OrganizationEntryHandler.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator.datatype.organization;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Organization;
+import com.liferay.portal.model.OrganizationConstants;
+import com.liferay.portal.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.tool.datamanipulator.entry.BaseEntry;
+import com.liferay.tool.datamanipulator.entry.EntryArgs;
+import com.liferay.tool.datamanipulator.entry.util.EntryUtil;
+import com.liferay.tool.datamanipulator.handler.entryhandler.AbstractEntryHandler;
+import com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel;
+import com.liferay.tool.datamanipulator.model.DataManipulator;
+import com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor;
+import com.liferay.tool.datamanipulator.service.DataManipulatorLocalServiceUtil;
+
+/**
+ * @author Tibor Kovács
+ *
+ */
+public class OrganizationEntryHandler extends AbstractEntryHandler implements
+		EntryHandlerModel {
+
+	/**
+	 * @param count
+	 * @param update
+	 * @param depth
+	 * @param subCount
+	 * @param baseEntry
+	 * @param subEntryHandler
+	 * @param requestProcessor
+	 */
+	public OrganizationEntryHandler(
+		int count, int update, int depth, int subCount, BaseEntry baseEntry,
+		EntryHandlerModel subEntryHandler, RequestProcessor requestProcessor) {
+
+		super(
+			count, update, depth, subCount, baseEntry, subEntryHandler,
+			requestProcessor);
+
+		_userIds = requestProcessor.getUserIds();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getCreateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public EntryArgs getCreateEntryArgs(
+			long parentId, String postString, RequestProcessor requestProcessor)
+		throws PortalException, SystemException {
+
+		EntryArgs args = new EntryArgs(requestProcessor);
+
+		args.setParameter("parentOrganizationId", parentId);
+		args.setParameter("name", "Test Organization" + postString + " Name");
+		args.setParameter(
+			"type", OrganizationConstants.TYPE_REGULAR_ORGANIZATION);
+
+		args.setParameter(
+			"statusId", GetterUtil.getInteger(PropsUtil.get(_statusId)));
+
+		return args;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getDataManipulatorFromObject(java.lang.Object)
+	 */
+	@Override
+	public DataManipulator getDataManipulatorFromObject(Object createdEntry)
+		throws PortalException, SystemException {
+
+		Organization org = (Organization)createdEntry;
+
+		UserLocalServiceUtil.addOrganizationUsers(
+			org.getOrganizationId(), _userIds);
+
+		Group group = org.getGroup();
+
+		return DataManipulatorLocalServiceUtil.addDataManipulator(
+			group.getGroupId(), Organization.class.getName(),
+			org.getOrganizationId());
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getUpdateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public EntryArgs getUpdateEntryArgs(
+			long entryId, String postString, RequestProcessor requestProcessor)
+		throws PortalException, SystemException {
+
+		Organization org = OrganizationLocalServiceUtil.getOrganization(
+			entryId);
+
+		EntryArgs args = new EntryArgs(requestProcessor);
+
+		args.setParameter("organizationId", entryId);
+		args.setParameter(
+			"parentOrganizationId", org.getParentOrganizationId());
+
+		args.setParameter(
+			"name", EntryUtil.getEditString(org.getName(), postString));
+
+		args.setParameter("type", org.getType());
+		args.setParameter("statusId", org.getStatusId());
+
+		return args;
+	}
+
+	private static String _statusId =
+		PropsKeys.
+			SQL_DATA_COM_LIFERAY_PORTAL_MODEL_LISTTYPE_ORGANIZATION_STATUS;
+
+	private long[] _userIds;
+
+}
