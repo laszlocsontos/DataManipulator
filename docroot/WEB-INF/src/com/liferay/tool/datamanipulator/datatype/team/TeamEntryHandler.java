3e74020761a12758d12bf3376a422122d735e380
tiborkovacs <tibor.kovacs@liferay.com>
2013-09-10 21:48:23 +0200

    Add Team generation

---
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/team/TeamEntryHandler.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.tool.datamanipulator.datatype.team;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.Team;
+import com.liferay.portal.service.TeamLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.tool.datamanipulator.entry.BaseEntry;
+import com.liferay.tool.datamanipulator.entry.EntryArgs;
+import com.liferay.tool.datamanipulator.entry.util.EntryUtil;
+import com.liferay.tool.datamanipulator.handler.entryhandler.AbstractEntryHandler;
+import com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel;
+import com.liferay.tool.datamanipulator.model.DataManipulator;
+import com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor;
+import com.liferay.tool.datamanipulator.service.DataManipulatorLocalServiceUtil;
+
+/**
+ * @author Tibor Kovács
+ *
+ */
+public class TeamEntryHandler extends AbstractEntryHandler implements
+		EntryHandlerModel {
+
+	/**
+	 * @param count
+	 * @param update
+	 * @param depth
+	 * @param subCount
+	 * @param baseEntry
+	 * @param subEntryHandler
+	 * @param requestProcessor
+	 */
+	public TeamEntryHandler(
+		int count, int update, int depth, int subCount, BaseEntry baseEntry,
+		EntryHandlerModel subEntryHandler, RequestProcessor requestProcessor) {
+
+		super(
+			count, update, depth, subCount, baseEntry, subEntryHandler,
+			requestProcessor);
+
+		_userIds = requestProcessor.getUserIds();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getCreateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public EntryArgs getCreateEntryArgs(
+			long parentId, String postString, RequestProcessor requestProcessor)
+		throws PortalException, SystemException {
+
+		EntryArgs args = new EntryArgs(requestProcessor);
+
+		args.setParameter("name", "Test Team" + postString + " name");
+		args.setParameter(
+			"description", "Test Team" + postString + " Description");
+
+		return args;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getDataManipulatorFromObject(java.lang.Object)
+	 */
+	@Override
+	public DataManipulator getDataManipulatorFromObject(Object createdEntry)
+		throws PortalException, SystemException {
+
+		Team team = (Team)createdEntry;
+
+		UserLocalServiceUtil.addTeamUsers(team.getTeamId(), _userIds);
+
+		return DataManipulatorLocalServiceUtil.addDataManipulator(
+			team.getGroupId(), Team.class.getName(), team.getTeamId());
+	}
+
+	/* (non-Javadoc)
+	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getUpdateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
+	 */
+	@Override
+	public EntryArgs getUpdateEntryArgs(
+			long entryId, String postString, RequestProcessor requestProcessor)
+		throws PortalException, SystemException {
+
+		Team team = TeamLocalServiceUtil.getTeam(entryId);
+
+		EntryArgs args = new EntryArgs(requestProcessor);
+
+		args.setParameter("teamId", entryId);
+		args.setParameter(
+			"name", EntryUtil.getEditString(team.getName(), postString));
+
+		args.setParameter(
+			"description",
+			EntryUtil.getEditString(team.getDescription(), postString));
+
+		return args;
+	}
+
+	private long[] _userIds;
+
+}
