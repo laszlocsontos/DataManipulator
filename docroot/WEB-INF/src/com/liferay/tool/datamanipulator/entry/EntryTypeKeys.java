379f6da8bd7ff573cc2103a1e1288e7ec116710b
tiborkovacs <tibor.kovacs@liferay.com>
2013-09-10 21:48:23 +0200

    Add Team generation

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/entry/EntryTypeKeys.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/entry/EntryTypeKeys.java
@@ -110,6 +110,8 @@ public class EntryTypeKeys {
 
 	public static final String PORTAL_ROLE = "role";
 
+	public static final String PORTAL_TEAM = "team";
+
 	public static final String PORTAL_SITE = "site";
 
 	public static final String PORTAL_USER = "user";
@@ -144,6 +146,7 @@ public class EntryTypeKeys {
 		ArrayList<String> types = new ArrayList<String>();
 
 		types.add(PORTAL_ORGANIZATION);
+		types.add(PORTAL_TEAM);
 		types.add(PORTAL_USER);
 
 		return types;
