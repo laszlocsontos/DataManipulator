1c2fe6ab1ab0e5d48d1b90ea90c86ba2c8839a21
tiborkovacs <tibor.kovacs@liferay.com>
2013-09-13 12:14:58 +0200

    Can generate private pages

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/layout/LayoutDisplayFields.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/layout/LayoutDisplayFields.java
@@ -52,6 +52,9 @@ public class LayoutDisplayFields {
 		fields.addUserMultiSelect();
 		fields.addSeparator();
 
+		fields.addCheckbox(EntryTypeKeys.GENERAL_LAYOUT_ENTRY);
+		fields.addSeparator();
+
 		fields.addCount(EntryTypeKeys.GENERAL_LAYOUT_ENTRY, true);
 		fields.addDepth(EntryTypeKeys.GENERAL_LAYOUT_ENTRY);
 		fields.addSubCount(EntryTypeKeys.GENERAL_LAYOUT_ENTRY);
