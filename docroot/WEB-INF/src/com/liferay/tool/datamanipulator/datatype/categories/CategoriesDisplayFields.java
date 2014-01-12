c1325f87a0b494964872722c4159781f6561b333
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-13 00:13:44 +0100

    Add parent Vocabulary select

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/categories/CategoriesDisplayFields.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/categories/CategoriesDisplayFields.java
@@ -14,13 +14,19 @@
 
 package com.liferay.tool.datamanipulator.datatype.categories;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.KeyValuePair;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portlet.asset.model.AssetCategoryConstants;
+import com.liferay.portlet.asset.model.AssetVocabulary;
+import com.liferay.portlet.asset.service.AssetVocabularyLocalServiceUtil;
 import com.liferay.tool.datamanipulator.displayfield.DisplayFields;
 import com.liferay.tool.datamanipulator.displayfield.Field;
 import com.liferay.tool.datamanipulator.displayfield.FieldKeys;
 import com.liferay.tool.datamanipulator.entry.EntryTypeKeys;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,14 +34,47 @@ import java.util.List;
  *
  */
 public class CategoriesDisplayFields {
-	public static List<Field> getDisplayFields() throws SystemException {
+	public static final String ASSET_VOCABULARIES_LIST = "vocabularies-list";
+	public static final String ASSET_VOCABULARY_ID = "vocabulary-id";
+
+	public static List<Field> getDisplayFields(long groupId)
+		throws SystemException, PortalException {
+
+		List<AssetVocabulary> vocabularyList =
+			AssetVocabularyLocalServiceUtil.getGroupVocabularies(groupId);
+
+		List<KeyValuePair> vocabulariesNameId = new ArrayList<KeyValuePair>(
+			vocabularyList.size() + 1);
+
+		vocabulariesNameId.add(
+			new KeyValuePair(
+				EntryTypeKeys.GENERAL_ASSET_CATEGORY + StringPool.DASH +
+					FieldKeys.DEFAULT_PARENT,
+				String.valueOf(
+					AssetCategoryConstants.DEFAULT_PARENT_CATEGORY_ID)));
+
+		for (AssetVocabulary vocabulary : vocabularyList) {
+			vocabulariesNameId.add(
+				new KeyValuePair(
+					vocabulary.getName(),
+					String.valueOf(vocabulary.getVocabularyId())));
+		}
+
 		DisplayFields fields = new DisplayFields();
 
 		fields.addUserMultiSelect();
 		fields.addSeparator();
 
-		fields.addCount(EntryTypeKeys.GENERAL_ASSET_VOCABULARY, true);
+		fields.addCount(EntryTypeKeys.GENERAL_ASSET_VOCABULARY);
 		fields.addUpdateLevel(EntryTypeKeys.GENERAL_ASSET_VOCABULARY);
+		fields.addInfo(EntryTypeKeys.GENERAL_ASSET_VOCABULARY + "-add-to-new");
+		fields.addSeparator();
+
+		fields.addSelectList(ASSET_VOCABULARIES_LIST, vocabulariesNameId);
+		fields.addInput(ASSET_VOCABULARY_ID);
+		fields.addInfo(
+			EntryTypeKeys.GENERAL_ASSET_VOCABULARY + "-add-to-exist");
+
 		fields.addSeparator();
 
 		fields.addCount(EntryTypeKeys.GENERAL_ASSET_CATEGORY);
