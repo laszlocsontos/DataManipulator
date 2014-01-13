0d1054edf2a9690d45d90c283851842e8df82462
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-13 08:41:24 +0100

    Add parent Bookmarks Folder select

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/bookmarks/BookmarksDisplayFields.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/bookmarks/BookmarksDisplayFields.java
@@ -15,10 +15,17 @@
 package com.liferay.tool.datamanipulator.datatype.bookmarks;
 
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portlet.bookmarks.model.BookmarksFolder;
+import com.liferay.portlet.bookmarks.model.BookmarksFolderConstants;
+import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
 import com.liferay.tool.datamanipulator.displayfield.DisplayFields;
 import com.liferay.tool.datamanipulator.displayfield.Field;
+import com.liferay.tool.datamanipulator.displayfield.FieldKeys;
 import com.liferay.tool.datamanipulator.entry.EntryTypeKeys;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,10 +33,43 @@ import java.util.List;
  *
  */
 public class BookmarksDisplayFields {
-	public static List<Field> getDisplayFields() throws SystemException {
+	public static final String BOOKMARKS_FOLDER_ID = "bookmarks-folder-id";
+	public static final String BOOKMARKS_FOLDER_LIST = "bookmarks-folder-list";
+
+	public static List<Field> getDisplayFields(long groupId)
+		throws SystemException {
+
+		List<BookmarksFolder> bookmarksFolderList =
+			BookmarksFolderLocalServiceUtil.getFolders(
+				groupId, BookmarksFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		List<KeyValuePair> bookmarksFolderNameId = new ArrayList<KeyValuePair>(
+			bookmarksFolderList.size() + 1);
+
+		bookmarksFolderNameId.add(
+			new KeyValuePair(
+				EntryTypeKeys.GENERAL_BOOKMARKS + StringPool.DASH +
+					FieldKeys.DEFAULT_PARENT,
+				String.valueOf(
+					BookmarksFolderConstants.DEFAULT_PARENT_FOLDER_ID)));
+
+		for (BookmarksFolder folder : bookmarksFolderList) {
+			bookmarksFolderNameId.add(
+				new KeyValuePair(
+					folder.getName(), String.valueOf(folder.getFolderId())));
+		}
+
 		DisplayFields fields = new DisplayFields();
 
 		fields.addUserMultiSelect();
+		fields.addSeparator();
+
+		fields.addSelectList(BOOKMARKS_FOLDER_LIST, bookmarksFolderNameId);
+		fields.addInput(
+			BOOKMARKS_FOLDER_ID,
+			String.valueOf(BookmarksFolderConstants.DEFAULT_PARENT_FOLDER_ID));
+
+		fields.addInfo(EntryTypeKeys.GENERAL_BOOKMARKS + "-add-to-exist");
 		fields.addSeparator();
 
 		fields.addCount(EntryTypeKeys.GENERAL_BOOKMARKS_FOLDER);
