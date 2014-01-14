98c0b2318c60cdfa4905e8359c0b1cc1937af82c
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-14 10:07:50 +0100

    Add parent Wiki Node select

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/wiki/WikiDisplayFields.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/wiki/WikiDisplayFields.java
@@ -14,13 +14,18 @@
 
 package com.liferay.tool.datamanipulator.datatype.wiki;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.KeyValuePair;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portlet.wiki.model.WikiNode;
+import com.liferay.portlet.wiki.service.WikiNodeLocalServiceUtil;
 import com.liferay.tool.datamanipulator.displayfield.DisplayFields;
 import com.liferay.tool.datamanipulator.displayfield.Field;
 import com.liferay.tool.datamanipulator.displayfield.FieldKeys;
 import com.liferay.tool.datamanipulator.entry.EntryTypeKeys;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,7 +33,29 @@ import java.util.List;
  *
  */
 public class WikiDisplayFields {
-	public static List<Field> getDisplayFields() throws SystemException {
+	public static final String WIKI_NODE_ID = "wiki-node-id";
+	public static final String WIKI_NODE_LIST = "wiki-node-list";
+
+	public static List<Field> getDisplayFields(long groupId)
+		throws SystemException, PortalException {
+
+		List<WikiNode> wikiNodeList = WikiNodeLocalServiceUtil.getNodes(
+			groupId);
+
+		List<KeyValuePair> wikiNodeNameId = new ArrayList<KeyValuePair>(
+			wikiNodeList.size() + 1);
+
+		wikiNodeNameId.add(
+			new KeyValuePair(
+				EntryTypeKeys.GENERAL_WIKI + StringPool.DASH +
+					FieldKeys.DEFAULT_PARENT, "0"));
+
+		for (WikiNode node : wikiNodeList) {
+			wikiNodeNameId.add(
+				new KeyValuePair(
+					node.getName(), String.valueOf(node.getNodeId())));
+		}
+
 		DisplayFields fields = new DisplayFields();
 
 		fields.addUserMultiSelect();
@@ -36,6 +63,11 @@ public class WikiDisplayFields {
 
 		fields.addCount(EntryTypeKeys.GENERAL_WIKI_NODE);
 		fields.addUpdateLevel(EntryTypeKeys.GENERAL_WIKI_NODE);
+		fields.addSeparator();
+
+		fields.addSelectList(WIKI_NODE_LIST, wikiNodeNameId);
+		fields.addInput(WIKI_NODE_ID);
+		fields.addInfo(EntryTypeKeys.GENERAL_WIKI + "-ad-to-exist");
 		fields.addSeparator();
 
 		fields.addCount(EntryTypeKeys.GENERAL_WIKI_PAGE);
