303938dd6a859fa71e66e9804df4dd40761d016e
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-14 13:51:00 +0100

    SourceFormat on User Handlers

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/user/UserDisplayFields.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/user/UserDisplayFields.java
@@ -26,6 +26,7 @@ import java.util.List;
  *
  */
 public class UserDisplayFields {
+
 	public static List<Field> getDisplayFields() throws SystemException {
 		DisplayFields fields = new DisplayFields();
 
