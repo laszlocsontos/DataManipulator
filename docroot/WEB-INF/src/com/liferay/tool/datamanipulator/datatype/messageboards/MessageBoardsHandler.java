07c9054e114d5a22e31dbc76f4574e5a3b1fc0d3
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-14 13:50:57 +0100

    SourceFormat on MessageBoards Handlers

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/messageboards/MessageBoardsHandler.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/messageboards/MessageBoardsHandler.java
@@ -32,15 +32,17 @@ import com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor;
  * @author Tibor Kovács
  *
  */
-public class MessageBoardsHandler extends AbstractPortletHandler implements
-		PortletHandlerModel {
+public class MessageBoardsHandler extends AbstractPortletHandler
+	implements PortletHandlerModel {
 
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel#startErase(com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
 	 */
+
 	@Override
 	public void startErase(RequestProcessor requestProcessor)
-			throws PortalException, SystemException {
+		throws PortalException, SystemException {
+
 		// TODO Auto-generated method stub
 
 	}
@@ -48,9 +50,10 @@ public class MessageBoardsHandler extends AbstractPortletHandler implements
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.portlethandler.model.PortletHandlerModel#startGenerate(com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
 	 */
+
 	@Override
 	public void startGenerate(RequestProcessor requestProcessor)
-			throws PortalException, SystemException {
+		throws PortalException, SystemException {
 
 		// MessageBoards Message
 
