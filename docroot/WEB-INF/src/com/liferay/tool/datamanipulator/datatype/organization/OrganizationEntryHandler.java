2474ba0f8ce683eb15f74ac284451a27982e8f80
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-14 13:50:58 +0100

    SourceFormat on Organization Handlers

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/organization/OrganizationEntryHandler.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/organization/OrganizationEntryHandler.java
@@ -36,8 +36,8 @@ import com.liferay.tool.datamanipulator.service.DataManipulatorLocalServiceUtil;
  * @author Tibor Kovács
  *
  */
-public class OrganizationEntryHandler extends AbstractEntryHandler implements
-		EntryHandlerModel {
+public class OrganizationEntryHandler extends AbstractEntryHandler
+	implements EntryHandlerModel {
 
 	/**
 	 * @param count
@@ -62,6 +62,7 @@ public class OrganizationEntryHandler extends AbstractEntryHandler implements
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getCreateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
 	 */
+
 	@Override
 	public EntryArgs getCreateEntryArgs(
 			long parentId, String postString, RequestProcessor requestProcessor)
@@ -85,6 +86,7 @@ public class OrganizationEntryHandler extends AbstractEntryHandler implements
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getDataManipulatorFromObject(java.lang.Object)
 	 */
+
 	@Override
 	public DataManipulator getDataManipulatorFromObject(Object createdEntry)
 		throws PortalException, SystemException {
@@ -102,6 +104,7 @@ public class OrganizationEntryHandler extends AbstractEntryHandler implements
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getUpdateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
 	 */
+
 	@Override
 	public EntryArgs getUpdateEntryArgs(
 			long entryId, String postString, RequestProcessor requestProcessor)
